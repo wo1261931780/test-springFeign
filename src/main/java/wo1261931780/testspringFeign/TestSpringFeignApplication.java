cdafab0e3a460edbff509f4e94f3abbc54b990ad
junw <wo1261931780@gmail.com>
2024-04-29 19:21:50 +0800

    初始化

---
+++ src/main/java/wo1261931780/testspringFeign/TestSpringFeignApplication.java
@@ -0,0 +1,13 @@
+package wo1261931780.testspringFeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringFeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringFeignApplication.class, args);
+	}
+
+}
